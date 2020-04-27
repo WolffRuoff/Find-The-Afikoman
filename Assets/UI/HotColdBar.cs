b4e6ae8df4d95d12cbaaff163068a66ad2a95313
Matt Gropper <mgropps17@gmail.com>
2020-04-27 17:55:23 -0400

    fixed HC bar

--- Assets/UI/HotColdBar.cs
+++ Assets/UI/HotColdBar.cs
@@ -6,22 +6,18 @@ using UnityEngine.UI;
 public class HotColdBar : MonoBehaviour
 {
     public float secondsUntilStart = 210f;
+    public float maxDist = 15f;
     public GameObject player;
-    public Sprite cold;
-    public Sprite hot;
 
     private static GameObject matzah;
     private static bool active = false;
     private Image hotCold;
-    private CanvasGroup cg;
-    private RectTransform rt;
+    private Color red = new Color(.55f, 0f, 0f, 1f);
+    private Color blue = new Color(0f, 0f, .55f, 1f);
 
     void Start()
     {
         hotCold = GetComponent<Image>();
-        cg = GetComponent<CanvasGroup>();
-        cg.alpha = 0;
-        rt = GetComponent<RectTransform>();
     }
 
     void Update()
@@ -29,44 +25,10 @@ public class HotColdBar : MonoBehaviour
         // wait until the matzah is chosen and received
         if (active)
         {
-            // wait 3.5 mins to start showing images
-            if (secondsUntilStart > 0)
-            {
-                secondsUntilStart -= Time.deltaTime;
-            }
-            else
-            {
-                cg.alpha = 1;
-
-                float dist = Vector3.Distance(player.transform.position, matzah.transform.position);
-                Debug.Log(dist);
-
-                if (dist >= 2f)
-                {
-                    if (dist > 8f)
-                    {
-                        dist = 8f;
-                    }
-
-                    hotCold.sprite = cold;
-                    float length = dist * 37.5f;
-                    rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, length);
-                    rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, length);
-                }
-                else
-                {
-                    if (dist < .5f)
-                    {
-                        dist = .5f;
-                    }
-                    float length = (1 / dist) * 200;
-
-                    hotCold.sprite = hot;
-                    rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, length);
-                    rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, length);
-
-                }
-            }
+            secondsUntilStart -= Time.deltaTime;
+            float dist = Vector3.Distance(player.transform.position, matzah.transform.position) - .75f;
+            Debug.Log(dist);
+            hotCold.color = (secondsUntilStart < 0) ? Color.Lerp(red, blue, (dist / maxDist)) : new Color(0, 0, 0, 0);
         }
     }
 
