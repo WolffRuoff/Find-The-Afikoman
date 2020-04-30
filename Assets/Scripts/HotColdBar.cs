471568842fd68527b2b0ec16aaaabefbbf65a5dc
Matt Gropper <mgropps17@gmail.com>
2020-04-29 21:24:29 -0400

    fixed timers and added text for HC Bar

--- Assets/Scripts/HotColdBar.cs
+++ Assets/Scripts/HotColdBar.cs
@@ -5,40 +5,56 @@ using UnityEngine.UI;
 
 public class HotColdBar : MonoBehaviour
 {
-    public float secondsUntilStart = 210f;
     public float maxDist = 15f;
     public GameObject player;
 
     private static GameObject matzah;
+    private static float secondsUntilStart = 210f;
     private static bool active = false;
     private Image hotCold;
+    private Text text;
     private Color red = new Color(.55f, 0f, 0f, 1f);
     private Color blue = new Color(0f, 0f, .55f, 1f);
 
     void Start()
     {
         hotCold = GetComponent<Image>();
-        if (ApplicationModel.findAll)
-        {
-            this.gameObject.SetActive(false);
-        }
+        text = transform.GetChild(0).GetComponent<Text>();
+        text.gameObject.SetActive(false);
     }
 
     void Update()
     {
+        hotCold.color = new Color(0, 0, 0, 0);
+
         // wait until the matzah is chosen and received
         if (active)
         {
             secondsUntilStart -= Time.deltaTime;
+            if (secondsUntilStart < 0)
+            {
+                text.gameObject.SetActive(true);
+            }
+
             float dist = Vector3.Distance(player.transform.position, matzah.transform.position) - .75f;
             Debug.Log(dist);
             hotCold.color = (secondsUntilStart < 0) ? Color.Lerp(red, blue, (dist / maxDist)) : new Color(0, 0, 0, 0);
+
+            if (dist < .6f)
+            {
+                text.text = "Hot";
+            }
+            else
+            {
+                text.text = "Cold";
+            }
         }
     }
 
-    public static void receiveMatzah(GameObject m)
+    public static void receiveMatzah(GameObject m, float seconds)
     {
         matzah = m;
+        secondsUntilStart = seconds;
         active = true;
     }
 }
